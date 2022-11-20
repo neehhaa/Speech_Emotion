82a5146ba40ea3c4951c131cf849fbbac9337e47
neehhaa <nehawasnik95@outlook.com>
2022-11-21 04:29:54 +0530

    youtube video sentiment analysis

--- speech_to_text/process.py
+++ speech_to_text/process.py
@@ -11,6 +11,7 @@ IV. SAVE TRANSCRIPT
 import requests
 from api_config import API_KEY_ASSEMBLYAI
 import time
+import json
 
 upload_endpoint = "https://api.assemblyai.com/v2/upload"
 transcript_endpoint = "https://api.assemblyai.com/v2/transcript"
@@ -36,8 +37,8 @@ def upload(filename):
 
 # II. TRANSCRIBE
 
-def transcribe(audio_url):
-    transcript_request = {"audio_url":audio_url}
+def transcribe(audio_url,sentiment_analysis):
+    transcript_request = {"audio_url":audio_url,'sentiment_analysis':sentiment_analysis}
     transcript_response = requests.post(transcript_endpoint, json = transcript_request, headers=headers)
     job_id = transcript_response.json()['id']
     return job_id
@@ -50,8 +51,8 @@ def poll(transcript_id):
     polling_response = requests.get(polling_endpoint, headers=headers)
     return polling_response.json()
 
-def get_transcription_result_url(audio_url):
-    transcribe_id = transcribe(audio_url)
+def get_transcription_result_url(audio_url,sentiment_analysis):
+    transcribe_id = transcribe(audio_url,sentiment_analysis)
     while True:
         data = poll(transcribe_id)
         if data['status'] == 'completed':
@@ -65,13 +66,19 @@ def get_transcription_result_url(audio_url):
 
 # IV. SAVE TRANSCRIPT
 
-def save_transcript(audio_url, filename):
+def save_transcript(audio_url, filename,sentiment_analysis=False):
     data,error = get_transcription_result_url(audio_url)
 
     if data:
         text_filename = filename + ".txt"
         with open(text_filename,"w") as f:
             f.write(data['text'])
+        if sentiment_analysis:
+            sen_filename = filename + "_sentiment.json"
+            with open(sen_filename,"w") as f:
+                sentiment = data["sentiment_analysis_results"]
+                json.dump(sentiment,f,indent=4)
+
         print('saved')
     elif error:
         print("Error",error)
